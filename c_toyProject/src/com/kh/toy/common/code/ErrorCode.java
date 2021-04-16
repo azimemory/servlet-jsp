c85db0d56a5431cc3f3de08d4168e75ee5858375
azimemory <killsky2014@naver.com>
2021-04-16 21:41:17 +0900

    code

--- c_toyProject/src/com/kh/toy/common/code/ErrorCode.java
+++ c_toyProject/src/com/kh/toy/common/code/ErrorCode.java
@@ -4,7 +4,7 @@ public enum ErrorCode {
 
 	DATABASE_ACCESS_ERROR("데이터베이스와 통신 중 에러가 발생하였습니다."),
 	
-	EXPRIATION_AUTH("이미 만료된 인증입니다."),
+	EXPIRATION_AUTH("이미 만료된 인증입니다."),
 	UNAUTHORIZED_PAGE("접근 권한이 없는 페이지 입니다."),
 	
 	MAIL_SENDING_ERROR("이메일 발송 중 에러가 발생하였습니다."),
